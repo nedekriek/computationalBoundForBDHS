be58483d76ede5d9f86ad55a86bc67a92699cd87
Henry Zwart <henrybzwart@gmail.com>
2022-03-13 13:12:58 +1300

    Move code to python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='computational_bounds',
+      version='0.1',
+      description='Computational Bounds for BDHS',
+      url='https://github.com/nedekriek/computationalBoundForBDHS',
+      author='Natasha de Kriek',
+      author_email='natasha.de.kriek@gmail.com',
+      license='MIT',
+      packages=['bounds'],
+      zip_safe=False)
